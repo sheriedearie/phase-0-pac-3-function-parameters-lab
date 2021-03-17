070fb48a09ba4175adf2525c03e3d00a41b6370d
Sheridan <sheridanmaustad@gmail.com>
2021-03-17 16:08:53 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(name){
+    return "Hi, my name is " + name + ".";
+}
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+function introductionWithLanguage(name, language) {
+    return "Hi, my name is " + name + " and I am learning to program in " + language + ".";
+}
+function introductionWithLanguageOptional(name, language) {
+    if (language == null) {
+      language = "JavaScript"; 
+    }
+    return "Hi, my name is " + name + " and I am learning to program in " + language + ".";
+}
